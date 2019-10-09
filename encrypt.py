8e1a131b3eeddbf4fbb87e8b7f61567661f551cb
Paweł Topór <pawel-topor5@wp.pl>
2019-10-09 18:58:41 +0200

    Fix encode problems

--- encrypt.py
+++ encrypt.py
@@ -3,8 +3,7 @@ import time
 from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
-# import pycryptodome
-from Crypto.Random import get_random_bytes
+# from Crypto.Random import get_random_bytes
 
 
 PORT = 8080
@@ -12,26 +11,27 @@ host = 'localhost'
 salt = b'k\x93nYL\x8a^\xcb\x04\xfe\x8aE\x07\xff\xc6\x92I\x08\xa4\xbe\xe9\x1c\xa7\x9f\xb2\xe3\rs}\x88\x02\xec'
 # print(get_random_bytes(32))
 
+
 def generateKey():
     # password = input("Please type encryption password")
     password = "test"
     key = PBKDF2(password, salt, dkLen=32)
     return key
 
+
 def get_message():
-    return "test"
-    # return input("Type message which you want to send")
+    return "test".encode('utf-8')
+    # return input("Type message which you want to send").encode('utf-8')
+
 
 def CBC_encryption(key, message):
     cipher = AES.new(key, AES.MODE_CBC)
     ciphered_data = cipher.encrypt(pad(message, AES.block_size))
-    print(cipher.iv)
-    print(cipher.iv + ciphered_data)
     return cipher.iv + ciphered_data
+
 
 def send_data(data, connection):
     connection.sendall(data)
-
 
 
 def create_connection():
@@ -45,11 +45,10 @@ def create_connection():
             print("Server niedostepny...Czekaj")
 
 
-
 if __name__== "__main__":
     connection = create_connection()
     key = generateKey()
-    message = get_message().encode()
+    message = get_message()
     data = CBC_encryption(key, message)
     send_data(data, connection)
 
